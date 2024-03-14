d384e536de14bd59da1127965c35f6cfe49dea7a
arjunguliya19 <arjunguliya19@gmail.com>
2024-03-14 16:00:19 -0400

    yes

---
+++ HDF5/features_2.py
@@ -0,0 +1,33 @@
+import h5py
+import numpy as np 
+from scipy.stats import skew, kurtosis 
+from sklearn.preprocessing import MinMaxScaler, StandardScaler 
+
+
+filename = "dataset.h5"
+
+
+def extract_features(segment): 
+    features = [] 
+    features.append(np.max(segment)) 
+    features.append(np.min(segment)) 
+    features.append(np.ptp(segment))  # peak-to-peak (range) 
+    features.append(np.mean(segment)) 
+    features.append(np.median(segment)) 
+    features.append(np.var(segment)) 
+    features.append(skew(segment)) 
+    features.append(kurtosis(segment)) 
+    features.append(np.sqrt(np.mean(np.square(segment)))) #rms 
+    features.append(np.sum(np.diff(np.sign(segment)) != 0) / len(segment))  # zero crossing rate 
+
+    return features 
+
+try:
+    with h5py.File(filename, "r") as f:
+        for segment in f['segments']:
+            features = extract_features(segment)
+            print(features)
+
+except Exception as e :
+    print("Dang")
+
