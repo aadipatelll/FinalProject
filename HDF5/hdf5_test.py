3d64d97b2614ff9fe856247d80a39c9d3f2db10a
Trevor Farias <levelonethousandr@gmail.com>
2024-03-14 17:15:42 -0400

    test

--- HDF5/hdf5_test.py
+++ HDF5/hdf5_test.py
@@ -17,14 +17,20 @@ def process_for_AI(df):
 ds_aadi = pd.read_csv('aadi_data.csv')
 ds_trevor = pd.read_csv('trevor_data.csv')
 ds_arjun = pd.read_csv('arjun_data.csv')
+ds_running = pd.read_csv('Test_walking.csv')
+ds_running = pd.read_csv('Test_jump.csv')
 
 # Process each member's CSV file into segments
 segments_aadi = process_for_AI(ds_aadi)
 segments_trevor = process_for_AI(ds_trevor)
 segments_arjun = process_for_AI(ds_arjun)
+segments_running = process_for_AI(ds_running)
+walk_set = pd.concat(segments_running)
+segments_jump = process_for_AI(ds_running)
+jump_set = pd.concat(segments_jump)
 
 # Concatenate all the shuffled segments from all members into a single list
-all_segments = segments_aadi + segments_trevor + segments_arjun
+all_segments = segments_aadi + segments_trevor + segments_arjun 
 
 # Shuffle the segments again to ensure random distribution of all members' segments
 np.random.shuffle(all_segments)
@@ -42,10 +48,14 @@ with h5py.File('dataset.h5', 'w') as hdf:
     # Create subgroups for training and testing sets in dataset group
     group_train = group_dataset.create_group('Train')
     group_test = group_dataset.create_group('Test')
+    group_test = group_dataset.create_group('walk')
+    group_test = group_dataset.create_group('jump')
     
     # Create and store data for the training and testing sets inside their respective groups
     group_train.create_dataset('data', data=train_set.to_numpy())
     group_test.create_dataset('data', data=test_set.to_numpy())
+    group_test.create_dataset('data', data=walk_set.to_numpy())
+    group_test.create_dataset('data', data=jump_set.to_numpy())
     
     # Create groups for each member at the root level
     group_aadi = hdf.create_group('aadi_data')
