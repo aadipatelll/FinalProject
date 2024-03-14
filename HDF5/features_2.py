47fac707053370f460ea66052d0e11e5dea415c4
arjunguliya19 <arjunguliya19@gmail.com>
2024-03-14 16:11:12 -0400

    updated features 2

--- HDF5/features_2.py
+++ HDF5/features_2.py
@@ -25,33 +25,26 @@ def extract_features(segment):
 
 try:
     with h5py.File('dataset.h5', 'r') as hdf:
-        # Read datasets
         train_set = hdf['dataset/Train'][:]
         test_set = hdf['dataset/Test'][:]
 
-    # Convert the numpy arrays to DataFrames
     df_train = pd.DataFrame(train_set)
     df_test = pd.DataFrame(test_set)
 
-    # Placeholder for features extracted from segments
     features_train = []
     features_test = []
 
-    # Process segments for train_set
     for start_row in range(0, df_train.shape[0], 250):
         segment = df_train.iloc[start_row:start_row+250]
         features_train.append(calculate_features(segment))
 
-    # Process segments for test_set 
     for start_row in range(0, df_test.shape[0], 250):
         segment = df_test.iloc[start_row:start_row+250]
         features_test.append(calculate_features(segment))
 
-    # Convert the lists of features into DataFrame
     df_features_train = pd.DataFrame(features_train)
     df_features_test = pd.DataFrame(features_test)
 
-    # Example: Save the features to CSV files
     df_features_train.to_csv('train_features.csv', index=False)
     df_features_test.to_csv('test_features.csv', index=False)
 
