a79ffd76ebb350adc8aeaa06a9c53adc8c52645b
arjunguliya19 <arjunguliya19@gmail.com>
2024-03-14 16:26:11 -0400

    featured updated

--- HDF5/features_2.py
+++ HDF5/features_2.py
@@ -5,49 +5,53 @@ from scipy.stats import skew, kurtosis
 from sklearn.preprocessing import MinMaxScaler, StandardScaler 
 
 
-filename = "dataset.h5"
-
-
 def extract_features(segment): 
-    features = [] 
-    features.append(np.max(segment)) 
-    features.append(np.min(segment)) 
-    features.append(np.ptp(segment))  # peak-to-peak (range) 
-    features.append(np.mean(segment)) 
-    features.append(np.median(segment)) 
-    features.append(np.var(segment)) 
-    features.append(skew(segment)) 
-    features.append(kurtosis(segment)) 
-    features.append(np.sqrt(np.mean(np.square(segment)))) #rms 
-    features.append(np.sum(np.diff(np.sign(segment)) != 0) / len(segment))  # zero crossing rate 
+    max_val = np.max(segment)
+    min_val = np.min(segment)
+    ptp_val = np.ptp(segment)  # peak-to-peak (range) 
+    mean_val = np.mean(segment)
+    median_val = np.median(segment)
+    var_val = np.var(segment)
+    skew_val = skew(segment)
+    kurt_val = kurtosis(segment) 
+    rms_val = np.sqrt(np.mean(np.square(segment))) #rms 
+    zero_val = np.sum(np.diff(np.sign(segment)) != 0) / len(segment)  # zero crossing rate 
 
-    return features 
 
-try:
-    with h5py.File('dataset.h5', 'r') as hdf:
-        train_set = hdf['dataset/Train'][:]
-        test_set = hdf['dataset/Test'][:]
 
-    df_train = pd.DataFrame(train_set)
-    df_test = pd.DataFrame(test_set)
+    return {
+        'max': max_val,
+        'min': min_val,
+        'ptp': ptp_val,
+        'mean': mean_val,
+        'median': median_val, 
+        'var': var_val, 
+        'skew': skew_val,
+        'kurt': kurt_val,
+        'rms': rms_val,
+        'zero': zero_val
+    }
 
-    features_train = []
-    features_test = []
+with h5py.File('dataset.h5', 'r') as hdf:
 
-    for start_row in range(0, df_train.shape[0], 250):
-        segment = df_train.iloc[start_row:start_row+250]
-        features_train.append(calculate_features(segment))
+    df_train = pd.DataFrame(hdf['dataset/Train'][:])
+    df_test = pd.DataFrame(hdf['dataset/Test'][:])
 
-    for start_row in range(0, df_test.shape[0], 250):
-        segment = df_test.iloc[start_row:start_row+250]
-        features_test.append(calculate_features(segment))
+features_train = []
+features_test = []
 
-    df_features_train = pd.DataFrame(features_train)
-    df_features_test = pd.DataFrame(features_test)
+for start_row in range(0, df_train.shape[0], 250):
+    segment = df_train.iloc[start_row:start_row+250]
+    features_train.append(extract_features(segment))
 
-    df_features_train.to_csv('train_features.csv', index=False)
-    df_features_test.to_csv('test_features.csv', index=False)
+for start_row in range(0, df_test.shape[0], 250):
+    segment = df_test.iloc[start_row:start_row+250]
+    features_test.append(extract_features(segment))
 
-except Exception as e :
-    print("Dang")
+df_features_train = pd.DataFrame(features_train)
+df_features_test = pd.DataFrame(features_test)
+
+df_features_train.to_csv('train_features.csv', index=False)
+df_features_test.to_csv('test_features.csv', index=False)
+
 
