bef0094649f0dbea54e2ab0e5dd3c5f58bd986c6
arjunguliya19 <arjunguliya19@gmail.com>
2024-03-27 19:18:24 -0400

    literally just changed a function name that i forgor to change in clean.py

--- HDF5/clean.py
+++ HDF5/clean.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # 
-def clean_and_convert_df(df):
+def clean_df(df):
     for col in df.columns:
         try:
             df[col] = df[col].str.extract(r'([-]?\d+\.\d+)').astype(float) # Extract floating point numbers and discard all other data
@@ -14,8 +14,8 @@ df_train = pd.read_csv('train_features.csv')
 df_test = pd.read_csv('test_features.csv')
 
 
-df_cleaned_test = clean_and_convert_df(df_test.copy())
-df_cleaned_train = clean_and_convert_df(df_train.copy())
+df_cleaned_test = clean_df(df_test.copy())
+df_cleaned_train = clean_df(df_train.copy())
 
 df_cleaned_train.to_csv('train_features_cleaned.csv', index=False)
 df_cleaned_test.to_csv('test_features_cleaned.csv', index=False)
